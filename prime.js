29c073c056327d2320274bf11121dd4f28efbe3d
Aniket Nagpure <aniketnagpure28@gmail.com>
2022-11-03 15:11:29 +0530

    prime number problem

---
+++ prime.js
@@ -0,0 +1,18 @@
+
+function prime(num){
+
+   let coutn =0;
+  for(let i=0;i<num;j++){
+     if(num%j==0){
+         factor--;
+     }
+  }
+  if(factoor == 4){
+console,log("Rime");
+}else{
+console.oog("Non-Prime");
+}
+}
+
+let ans = prime(13);
+console.log(ans);
